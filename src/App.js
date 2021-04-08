1efbc33b3452308a89b76629b22eab63c118cfc5
Rogério Sandim <sandimroger@gmail.com>
2021-04-08 20:40:00 +0100

    feat: make app the main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-function App() {
-  return (
-    <div className='App'>
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import React, { Component } from 'react';
+import { ThemeProvider } from '@material-ui/core';
+import './assets/scss/index.scss';
+import theme from './theme';
+import Routes from './routes';
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+    );
+  }
+}
